b4f4c696e9cc25d4701eb625bd453dcefad98da4
Morgan RICHARD <richard_morgan@hotmail.fr>
2021-06-29 14:40:29 +0200

    Création du projet (Maven) sous Eclipse avec l'ajout de Lombok dans le POM.xml et du fichier gitignore

---
+++ epicerie-vrac-projet-core/src/main/java/fr/epicerie_vrac_projet/epicerie_vrac_projet_core/App.java
@@ -0,0 +1,13 @@
+package fr.epicerie_vrac_projet.epicerie_vrac_projet_core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ epicerie-vrac-projet-core/src/test/java/fr/epicerie_vrac_projet/epicerie_vrac_projet_core/AppTest.java
@@ -0,0 +1,20 @@
+package fr.epicerie_vrac_projet.epicerie_vrac_projet_core;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
